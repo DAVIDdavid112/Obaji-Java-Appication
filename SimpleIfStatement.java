f6de760b53706f931bb32d592deda54160deb36c
DAVIDdavid112 <ifeanyiegejuru15@gmail.com>
2025-03-24 16:05:17 +0100

    operator

---
+++ SimpleIfStatement.java
@@ -0,0 +1,10 @@
+public class SimpleIfStatement{
+    public static void main(string[] args) {
+	    byte age = 12;
+		
+		if(age >= 18) {
+		     System.out.printf("You are an Adult");
+		}
+    }
+}	
+	
